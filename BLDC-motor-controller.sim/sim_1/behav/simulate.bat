d53079f37e3953fa4217236d5da19f787850504e
Per Breum <lillep3r@hotmail.com>
2017-10-23 20:39:01 +0200

    added TB for combined test of PWM and counter component. Seems to work

--- BLDC-motor-controller.sim/sim_1/behav/simulate.bat
+++ BLDC-motor-controller.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Vivado\\Vivado\\2017.2\\bin
-call %xv_path%/xsim PWM_generator_tb_behav -key {Behavioral:sim_1:Functional:PWM_generator_tb} -tclbatch PWM_generator_tb.tcl -log simulate.log
+call %xv_path%/xsim tb_counter_and_pwm_behav -key {Behavioral:sim_1:Functional:tb_counter_and_pwm} -tclbatch tb_counter_and_pwm.tcl -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
--- BLDC-motor-controller.srcs/sources_1/bd/BLDC_design/hdl/BLDC_design_wrapper.vhd
+++ BLDC-motor-controller.srcs/sources_1/bd/BLDC_design/hdl/BLDC_design_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2017.2 (win64) Build 1909853 Thu Jun 15 18:39:09 MDT 2017
---Date        : Mon Oct 23 18:15:00 2017
+--Date        : Mon Oct 23 18:42:01 2017
 --Host        : DESKTOP-NHGQ0HT running 64-bit major release  (build 9200)
 --Command     : generate_target BLDC_design_wrapper.bd
 --Design      : BLDC_design_wrapper
